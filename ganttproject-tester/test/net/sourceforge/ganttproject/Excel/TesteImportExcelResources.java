3819ee617c2a9211bc0586f954aaa7e34632afd8
rcosta349 <rafael.costa2109@gmail.com>
2022-11-29 13:30:29 +0000

    criação do ficheiro para o teste unitario da importação das resources através de excel

---
+++ ganttproject-tester/test/net/sourceforge/ganttproject/Excel/TesteImportExcelResources.java
@@ -0,0 +1,44 @@
+/*
+Copyright 2014 BarD Software s.r.o
+
+This file is part of GanttProject, an opensource project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package net.sourceforge.ganttproject.task.algorithm;
+
+import java.math.BigDecimal;
+
+import net.sourceforge.ganttproject.TestSetupHelper;
+import net.sourceforge.ganttproject.TestSetupHelper.TaskManagerBuilder;
+import net.sourceforge.ganttproject.resource.HumanResource;
+import net.sourceforge.ganttproject.task.Task;
+import net.sourceforge.ganttproject.task.TaskContainmentHierarchyFacade;
+import net.sourceforge.ganttproject.test.task.TaskTestCase;
+
+/**
+ * Tests to simulate Tasks High Lighting when they are complete
+ *
+ * @author Grupo abelhinha
+ */
+public class TesteImportExcelResources extends TaskTestCase {
+
+    @Test
+    void testImportExcelResources() {
+      ExcelImporterSys sys = new ExcelImporterSys("test.xlsx");
+      //asserts....
+    }
+
+
+}
